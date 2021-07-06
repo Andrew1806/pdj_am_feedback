9242eacb1e049cae8bf38f7ed9ec23105c6ca8cd
Andrew1806 <40208050+Andrew1806@users.noreply.github.com>
2021-07-06 11:19:24 +0100

    Adding a view to store data in session

--- reviews/views.py
+++ reviews/views.py
@@ -32,12 +32,14 @@ class ReviewListView(ListView):
     model = Review
     context_object_name = "reviews"
 
-    # def get_queryset(self):
-    #     base_query = super().get_queryset()
-    #     data = base_query.filter(rating__gt=4)
-    #     return data
-
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)  # Could use reverse
